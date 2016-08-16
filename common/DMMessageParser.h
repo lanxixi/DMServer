1eeeed363c83e26f6cbeea0f4a139140b38d8b74
bing <563853086@qq.com>
2016-08-15 20:08:06 -0700

    addfile

---
+++ common/DMMessageParser.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "DMMessage.h"
+#include "amqpcpp/include/message.h"
+
+class DMMessageParser
+{
+public:
+	enum DataSize
+	{
+		HEAD_BIT_LEN = 128,
+		HEAD_CHAR_LEN = 8,
+		CHAR_BIT_LEN = 8
+	};	
+    
+	DMMessageHead parse(char* begin);
+    
+    int parse(DMMessage& out, const AMQP::Message &in);
+    
+	int pack(DMMessage& mesg, char* buf);
+
+protected:
+private:
+	template<typename T> void DMGetBitData(char *src,T *dsc,int bit_s,int bit_e);
+};
